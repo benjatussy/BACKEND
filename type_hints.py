dcc1ed373bdc7f8e0e409fdb773b4d3833a04f11
benjatussy <benja.contrerasg@duocuc.cl>
2025-07-02 19:09:10 -0400

    Primer commit en backen

---
+++ type_hints.py
@@ -0,0 +1,16 @@
+
+my_string_variable = "my_string_variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_typed_variable: str = "my tiped string variable"
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+my_typed_variable = 5
+print(my_typed_variable)
+print(type(my_typed_variable))aaaa
