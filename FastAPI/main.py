01f6bd494c743295ed661e6a9e98e1c76ec73257
benjatussy <benja.contrerasg@duocuc.cl>
2025-07-04 16:30:50 -0400

    v2

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -5,6 +5,17 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, javierex!"}
 
+#URL LOCAL http://127.0.0.1:8000 
+
+@app.get("/url")
+async def url():
+    return {"url_curso": "https://www.youtube.com"}
+
+#URL LOCAL http://127.0.0.1:8000/url
+
+
+#documentation http://127.0.0.1:8000/docs
+#documentation http://127.0.0.1:8000/redoc
 #python -m uvicorn main:app --reload 
