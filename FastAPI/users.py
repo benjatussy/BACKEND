93187800e7da834d4085917a09ae74a0c92aa35a
benjatussy <benja.contrerasg@duocuc.cl>
2025-07-08 18:46:36 -0400

    V4_USER

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi import HTTPException
 
 app = FastAPI()
 #python -m uvicorn users:app --reload 
@@ -33,6 +34,51 @@ async def user(id: int):
 @app.get("/user/")
 async def user(id: int):
     return search_user(id)
+
+@app.post("/user/",response_model=User,status_code=201)
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        raise HTTPException(status_code=404, detail="El usuario ya existe")
+    user_list.append(user)
+    return user
+        
+
+@app.delete("/user/{id}")
+async def user(id: int):
+    found = False
+    for index, saved_user in enumerate(user_list):
+        if saved_user.id == id:
+            del user_list[index]
+            found = True
+            
+    
+    if not found:
+        return {"error": "El usuario no existe"}
+    return {"message": "Usuario eliminado correctamente"}
+
+#del elimina el elemento de la lista en la posición indicada, 
+#liberando ese espacio y quitando el usuario de la lista.
+
+
+@app.put("/user/")
+async def user(user: User):
+    
+    found = False
+    
+    for index, saved_user in enumerate(user_list):
+        if saved_user.id == user.id:
+            user_list[index] = user
+            found = True
+    
+    if not found:
+        return {"error": "El usuario no se ha actualizado"}
+    return user
+# Explicación:
+# - enumerate(user_list): permite recorrer la lista user_list obteniendo tanto el índice (index) como el usuario (saved_user) en cada iteración.
+# - index: es la posición actual en la lista, útil para modificar el elemento directamente.
+# - found = False: se usa como bandera para saber si se encontró y actualizó el usuario.
+# - found = True: se pone en True cuando se encuentra y actualiza el usuario.
+# Si found sigue siendo False al final, significa que no se encontró el usuario y se devuelve un error.
     
 def search_user(id: int):
     users = filter(lambda user: user.id == id, user_list)
