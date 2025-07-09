190fd69d055d7da4785ac08178097f890e5abddc
benjatussy <benja.contrerasg@duocuc.cl>
2025-07-08 21:33:58 -0400

    terminado routers de products y users

--- FastAPI/routers/products.py
+++ FastAPI/routers/products.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI, APIRouter
 
-router = APIRouter()
+router = APIRouter(prefix="/products"
+                   , tags=["products"]
+                   , responses={404: {"description": "Not found"}})
 
-@router.get("/products")
+@router.get("/")
 async def products():
-    return ["Product 1", "Product 2", "Product 3"]
+    return products_list
 
+products_list = [{
+    "Product 1",
+    "Product 2",
+    "Product 3"
+}
+]
 
-
+@router.get("/{id}")
+async def products():
+    return products_list[id]
 # Permite ejecutar este archivo como una API independiente
 app = FastAPI()
 app.include_router(router)
