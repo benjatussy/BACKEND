fd58c899cedb42c099713e51b4e762aa55354975
benjatussy <benja.contrerasg@duocuc.cl>
2025-07-16 16:19:51 -0400

    v6

--- FastAPI/routers/basic_auth_users.py
+++ FastAPI/routers/basic_auth_users.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, APIRouter
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
-
-app = FastAPI()
+router = APIRouter()
 
 class User(BaseModel):
     username: str
@@ -19,19 +18,18 @@ class UserDB(User):
     
 users_db = {
     "GASTON": {
-    "username": "gaston",
-    "fullname": "gastonto",
-    "email": "gaston@gmail.com",
-    "disabled": False,
-    "password": "123456"
+        "username": "gaston",
+        "fullname": "gastonto",
+        "email": "gaston@gmail.com",
+        "disabled": False,
+        "password": "123456"
     },
-    
     "siu": {
-    "username": "CRISHTIANO",
-    "fullname": "CRISTIANO RONALDO",
-    "email": "cris_7@gmail.com",
-    "disabled": True,
-    "password": "cr7goat"
+        "username": "CRISHTIANO",
+        "fullname": "CRISTIANO RONALDO",
+        "email": "cris_7@gmail.com",
+        "disabled": True,
+        "password": "cr7goat"
     }
 }
 
@@ -42,15 +40,16 @@ def search_user(username: str):
 async def current_user(token: str = Depends(oauth2)):
     user = search_user(token)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciales de autenticación invalidas", 
-                            headers={"WWW-Authenticate": "Bearer"}
-                            )
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales de autenticación inválidas", 
+            headers={"WWW-Authenticate": "Bearer"}
+        )
     return user
-    
 
-@app.post("/login")
+@router.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
-    user_db=users_db.get(form.username)
+    user_db = users_db.get(form.username)
     if not user_db:  
         raise HTTPException(status_code=400, detail="El usuario no es correcto")
     
@@ -60,9 +59,10 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
     
     return {"access_token": user.username, "token_type": "bearer"}
 
-@app.get("/users/me")
+@router.get("/users/me")
 async def me(user: User = Depends(current_user)):
     return user
-    
-    
-    
+
+@router.get("/users")
+async def get_users():
+    return [{"username": "test"}]
